d058d09e66c33efe89cfbfe57f693f4f9908cdf3
Dominichigozie2 <chigoziedomnic@gmail.com>
2025-03-11 11:54:42 +0100

    made new changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-const menuIcon = document.querySelector(".menu-icon");
+const menuIcon = document.querySelector("#bars");
 menuIcon.addEventListener("click", ()=>{
-    const sideNav = document.querySelector(".side-bar");
-    const menubar = document.querySelector(".menu-icon i");
+    const sideNav = document.querySelector(".sidebar");
+    const menubar = document.querySelector("#bars");
     sideNav.classList.toggle("active");
     menubar.classList.toggle("fa-times");
 
